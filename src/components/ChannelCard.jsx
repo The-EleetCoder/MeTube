6897f4b0cecad400d2d993826e560aefe3009243
The-EleetCoder <jaijain1803@gmail.com>
2023-09-30 03:04:21 +0530

    videos on feed

---
+++ src/components/ChannelCard.jsx
@@ -0,0 +1,19 @@
+import { Box, CardContent, CardMedia, Typography } from "@mui/material";
+import { CheckCirle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { demoProfilePicture } from "../utils/constants";
+
+const ChannelCard = () => {
+  return (
+    <Box
+      sx={{
+        boxShadow: 'none',
+        borderRadius: '20px',
+      }}
+    >
+
+    </Box>
+  )
+};
+
+export default ChannelCard;
