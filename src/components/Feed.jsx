ef1e4a8e67533bf49a85aa1dbb991f9a7ca06fbd
The-EleetCoder <jaijain1803@gmail.com>
2023-09-29 18:52:07 +0530

    sidebar and feed section

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,19 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { Box, Stack, Typography } from "@mui/material";
-import Sidebar from "./Sidebar";
+import { Sidebar, Videos } from "./";
 
 const Feed = () => {
   return (
-    <Stack sx={{flexDirection:{sx: "column", md: "row"}}}>
-      <Box sx={{ height: { sx: "auto", md: "92vh" }, borderRight: "1px solid #3d3d3d", px: { sx: 0, md: 2 } }}>
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      {/* for sidebar */}
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          px: { sx: 0, md: 2 },
+        }}
+      >
         <Sidebar />
-        
-        <Typography className="copyright" variant="body2" sx={{ mt: 1.5, color: "#fff", }}>
+
+        <Typography
+          className="copyright"
+          variant="body2"
+          sx={{ mt: 1.5, color: "#fff" }}
+        >
           Copyright © 2023 MeTube
         </Typography>
       </Box>
-    </Stack>
-  )
-}
 
-export default Feed
+      {/* for videos */}
+      <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontWeight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#F31503" }}>videos</span>
+        </Typography>
+
+        <Videos videos={[]} />
+      </Box>
+    </Stack>
+  );
+};
+
+export default Feed;
