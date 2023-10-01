9060d6c82de91d7beaacd08613bdd67835498bf8
The-EleetCoder <jaijain1803@gmail.com>
2023-10-01 16:44:46 +0530

    search functionality

--- src/components/ChannelDetail.jsx
+++ src/components/ChannelDetail.jsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box } from "@mui/material";
+
+import { Videos, ChannelCard } from "./";
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const ChannelDetail = () => {
-  return (
-    <div>ChannelDetail</div>
-  )
-}
+  const [channelDetail, setChannelDetail] = useState(null);
+  const [videos, setVideos] = useState([]);
 
-export default ChannelDetail
+  const { id } = useParams();
+  useEffect(() => {
+    fetchFromAPI(`channels?part=snippet&id=${id}`).then((data) =>
+      setChannelDetail(data?.items[0])
+    );
+
+    fetchFromAPI(`search?channelId=${id}&part=snippet&order=date`).then(
+      (data) => setVideos(data?.items)
+    );
+  }, [id]);
+  return (
+    <Box minHeight="95vh">
+      <Box>
+        <div
+          style={{
+            background: 'linear-gradient(169deg, rgba(0,0,0,1) 0%, rgba(55,102,165,1) 72%, rgba(101,123,176,1) 100%, rgba(255,255,255,1) 100%)',
+            zIndex: 10,
+            height: '300px'
+          }}
+        />
+        <ChannelCard channelDetail={channelDetail} marginTop='-110px' />
+      </Box>
+      <Box display="flex" p="2">
+          <Box sx={{ mr: {sm: '100px'}}}/>
+          <Videos videos={videos}/>
+      </Box>
+    </Box>
+  )
+};
+
+export default ChannelDetail;
